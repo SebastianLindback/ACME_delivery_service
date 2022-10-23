3e3054880baeed80846194ff6603ccd103f4c4d0
Sebastian <sebbe.lindback@hotmail.com>
2022-10-23 16:43:51 +0200

    Update DeliveryCost.tsx

--- client/src/components/DeliveryCost.tsx
+++ client/src/components/DeliveryCost.tsx
@@ -7,33 +7,36 @@ function DeliveryCost() {
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setInput(event.target.value);
   };
-  const result = CalculateRoute(input.toUpperCase());
-  // Calculate the cost by adding all the matching routes
-  let sum = 0;
-  result.forEach(route => {
-    sum += route?.cost! 
-  });
   
-  return (<>
+  const formatInput = (input: string) => {
+    if (!input.includes("-")) input = input.substring(0,1) + "-" + input.substring(2,1);
+    return input.toUpperCase();;
+  }
+
+  const result = CalculateRoute(formatInput(input));
+  
+  
+  
+  return (
     <div className='row col-sm-12  col-lg-8 d-flex flex-colum justify-content-center mx-auto'>
-      <label className="col-6 text-center"><strong>Delivery cost: </strong></label>
+      <label className="col-6 text-center"><strong>Delivery cost for route: </strong></label>
       <input
         className="col-6"
         type="text"
         onChange={handleChange}
+        placeholder={'For example "A-C"'}
         value={input}>
       </input>
     
       <div className='row col-12 d-flex flex-colum justify-content-center mx-auto'>
-      <div className='alert alert-dark col-12 text-center'>
-        {(result.length > 0)
-        ? <span> The delivery cost for route <strong> {input.toUpperCase()} </strong> is <strong>{sum}</strong></span>
-        : <strong>No Such Route</strong>}
-        </div>
+        <div className='alert alert-dark col-12 text-center'>
+          {(result)
+          ? <span> The delivery cost for route <strong> {result.node_ofOrigin + "-" + result.node_ofDestination} </strong> is <strong>{result.cost}</strong></span>
+          : <strong>No Such Route</strong>}
+          </div>
+      </div>
     </div>
-    </div>
-    
-    </>)
+    )
 }
 
 export default DeliveryCost
